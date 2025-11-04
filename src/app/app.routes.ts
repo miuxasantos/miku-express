7a46e2e484e211df45a4d2747e34fb1b640e53a8
Miuxa Santos <miuxasantos06@gmail.com>
2025-11-04 20:13:53 -0300

    feat(rotas): começando o processo da criação de rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,9 +6,34 @@ export const routes: Routes = [
     loadChildren: () =>
       import('./dashboard/home/home.routes').then(m => m.HOME_ROUTES)
   },
-  // {
-  //   path: 'clientes',
-  //   loadChildren: () =>
-  //     import('./cadastros/cliente/cliente.routes').then(m => m.CLIENTES_ROUTES)
-  // },
+  {
+    path: 'rastreio',
+    loadChildren: () =>
+      import('./rastreio/rastreio.component.routes').then(m => m.RASTREIO_ROUTES)
+  },
+  {
+    path: 'meuspacotes',
+    loadChildren: () =>
+      import('./pacotes/pacotes.routes').then(m => m.MEUS_PACOTES_ROUTES)
+  },
+  {
+    path: 'contato',
+    loadChildren: () =>
+      import('./contato/contato.routes').then(m => m.CONTATO_ROUTES)
+  },
+  {
+    path: 'signin',
+    loadChildren: () =>
+      import('./login/signin/signin.routes').then(m => m.SIGNIN_ROUTES)
+  },
+  {
+    path: 'signup',
+    loadChildren: () =>
+      import('./login/signup/signup.routes').then(m => m.SIGNUP_ROUTES)
+  },
+  {
+    path: 'adm',
+    loadChildren: () =>
+      import('./admin/adm.routes').then(m => m.ADM_ROUTES)
+  }
 ];
